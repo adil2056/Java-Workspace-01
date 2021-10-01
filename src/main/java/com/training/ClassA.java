8fe9ab47b000e29ece041e23ba36327cc876d6ed
Ajay <ajayonline-03@yahoo.com>
2021-10-01 16:02:12 +0530

    First commit

---
+++ src/main/java/com/training/ClassA.java
@@ -0,0 +1,9 @@
+package com.training;
+
+import java.util.Date;
+
+public class ClassA {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the World of Java Training on - "+ new Date() );
+	}
+}
